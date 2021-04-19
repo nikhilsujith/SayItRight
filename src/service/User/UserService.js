6ba26305a973d7a5f05800ca74d3ea192ff35db9
deeppatel <deepbipinbhai.patel@mavs.uta.edu>
2021-04-18 20:35:15 -0500

    Page redirection for new user

--- src/service/User/UserService.js
+++ src/service/User/UserService.js
@@ -1,36 +1,14 @@
 // API
 
 // get user by poolid
-export const getUserByPoolId = (id) => {
-  const tempId = "p1";
-  try {
-    const url = `https://say-it-right.herokuapp.com/api/v1/user/${tempId}`;
-    return fetch(url).then((data) => data.json());
-    // .then(data => console.log(data))
-  } catch (error) {
-    alert("Fetch Group Error");
-    console.log(error);
-  }
+export const getUserByPoolId = async (id) => {
+     //setTimeout(async() => {
+        const response = await fetch('https://say-it-right.herokuapp.com/api/v1/user/'+id);
+                  const body = await response.json();
+                  const status=await response.status
+                  return {'status':status,'body':body};
+    //}, 5000);
 };
-
-//(id) => {
-//  const tempId = "p1";
-//  console.log(id)
-//  try {
-//    const url = 'https://say-it-right.herokuapp.com/api/v1/user/p1';
-//    return fetch('https://say-it-right.herokuapp.com/api/v1/user/p1')
-//               .then((response) => response.json())
-//               .then((json) => {
-//                 return json.status;
-//               })
-//               .catch((error) => {
-//                 console.error(error);
-//               });
-//  } catch (error) {
-//    alert("Get user by poolid Error");
-//    console.log(error);
-//  }
-//};
 
 // fetch enrolled groups
 export const fetchEnrolledGroups = (id) => {
