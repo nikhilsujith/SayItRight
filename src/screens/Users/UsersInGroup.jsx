e1314a8dd4bc237bc2b6f222e585d252502f983e
Nikhil Sujith <65097663+nikhilsujith@users.noreply.github.com>
2021-05-04 12:14:03 -0400

    UI updates

--- src/screens/Users/UsersInGroup.jsx
+++ src/screens/Users/UsersInGroup.jsx
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { View, StatusBar, StyleSheet, Text, Image, Alert, Button } from "react-native";
+import {
+  View,
+  StatusBar,
+  StyleSheet,
+  Text,
+  Image,
+  Alert,
+  Button,
+} from "react-native";
 import {
   Card,
   CardItem,
@@ -12,32 +20,18 @@ import {
   Icon,
 } from "native-base";
 import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
-import { exitGroup, fetchUsersInGroup, removeGroup, removeGroupMember } from "../../service/Group/GroupService";
+import {
+  exitGroup,
+  fetchUsersInGroup,
+  removeGroup,
+  removeGroupMember,
+} from "../../service/Group/GroupService";
 import { AccordianPack, LoadingIndicator } from "../../components";
 import { defaultOrImage } from "../../util";
 import { currentSession } from "../../util/AmplifyCurrentSession";
-// const GroupHeader = ({ navigation, title }) => {
-//   return (
-//     <Card style={styles.root}>
-//       <CardItem style={styles.root}>
-//         {/* <Left>
-//           <Icon name="close" onPress={() => navigation.goBack()} />
-//         </Left> */}
-//         <Body
-//           style={{ flex: 5, alignItems: "center", justifyContent: "center" }}
-//         >
-//           <Text>{title}</Text>
-//         </Body>
-//         {/* <Right style={{ flex: 1 }}>
-//           <Icon name="close" onPress={() => navigation.goBack()} />
-//         </Right> */}
-//       </CardItem>
-//     </Card>
-//   );
-// };
 
 const UsersInGroup = ({ navigation, route }) => {
-  const { id, groupName, groupDesc, groupImage,owned } = route.params;
+  const { id, groupName, groupDesc, groupImage, owned } = route.params;
   const [users, setUsers] = useState("");
   const [groupOwner, setGroupOwner] = useState(false);
   const [groupNameState, setGroupNameState] = useState("My Groups");
@@ -57,35 +51,30 @@ const UsersInGroup = ({ navigation, route }) => {
   const NameCard = ({ press, image, name, meaning, navigation, poolId }) => {
     let link = defaultOrImage(image);
     return (
-<TouchableOpacity onLongPress={() => {owned?
-                          removeGroupMemberFunc(poolId):null
-                        }} onPress={() => navigation.navigate('UserInformation', {
-                userName: name,
-                id: poolId
-              })}>
+      <TouchableOpacity
+        onLongPress={() => {
+          owned ? removeGroupMemberFunc(poolId) : null;
+        }}
+        onPress={() =>
+          navigation.navigate("UserInformation", {
+            userName: name,
+            id: poolId,
+          })
+        }
+      >
         <List style={{ padding: 10 }}>
           <ListItem thumbnail>
-
             <Left>
               <Thumbnail circular source={link} />
             </Left>
             <Body>
               <Text>{name}</Text>
-              {/* <Text note numberOfLines={1}>
-                  {''}
-                </Text> */}
             </Body>
-
-{/*                 <TouchableOpacity onPress={()=>removeGroupMemberFunc(poolId)} > */}
-{/*                 <Icon name='trash' /> */}
-{/*                  </TouchableOpacity> */}
           </ListItem>
-
         </List>
-</TouchableOpacity>
+      </TouchableOpacity>
     );
   };
-
 
   const UserList = () => {
     return (
@@ -100,7 +89,6 @@ const UsersInGroup = ({ navigation, route }) => {
                   name={object.fullName}
                   meaning={object.nameMeaning}
                   navigation={navigation}
-                  // press={() => navigation.navigate('UserInformation')}
                 />
               );
             })
@@ -112,65 +100,68 @@ const UsersInGroup = ({ navigation, route }) => {
     );
   };
 
-  const exitGroupFunc=()=>{
-  Alert.alert(
-        "Are you sure you want to exit the group?",
-        "",
-        [
-          { text: "OK", onPress: async () => {
-               const response = await exitGroup(id, currentUser);
-               const { status } = response;
-               if (status == 200){
-                 navigation.goBack();
-               }
-             }},
-           {
-             text: "Cancel"
-           }
-        ]
-      );
-  }
-
-  const removeGroupFunc=()=>{
-    Alert.alert(
-          "Are you sure you want to delete the group?",
-          "",
-          [
-            { text: "OK", onPress: async () => {
-                 const response = await removeGroup(id, currentUser);
-                 const { status } = response;
-                 if (status == 200){
-                   navigation.goBack();
-                 }
-               }},
-             {
-               text: "Cancel"
-             }
-          ]
-        );
-    }
-
-  function removeGroupMemberFunc(user_poolId){
-        //console.log(x)
-          Alert.alert(
-                "Are you sure you want to remove this user from the group?",
-                "",
-                [
-                  { text: "OK", onPress: async () => {
-                       const response = await removeGroupMember(currentSession(),route.params.id,user_poolId);
-                       //const { status } = response.status;
-                       //alert(response.status)
-                       if (response.status == 200){
-                         alert("User removed successfully!")
-                         navigation.goBack();
-                       }
-                     }},
-                   {
-                     text: "Cancel"
-                   }
-                ]
-              );
+  const exitGroupFunc = () => {
+    Alert.alert("Are you sure you want to exit the group?", "", [
+      {
+        text: "OK",
+        onPress: async () => {
+          const response = await exitGroup(id, currentUser);
+          const { status } = response;
+          if (status == 200) {
+            navigation.goBack();
           }
+        },
+      },
+      {
+        text: "Cancel",
+      },
+    ]);
+  };
+
+  const removeGroupFunc = () => {
+    Alert.alert("Are you sure you want to delete the group?", "", [
+      {
+        text: "OK",
+        onPress: async () => {
+          const response = await removeGroup(id, currentUser);
+          const { status } = response;
+          if (status == 200) {
+            navigation.goBack();
+          }
+        },
+      },
+      {
+        text: "Cancel",
+      },
+    ]);
+  };
+
+  function removeGroupMemberFunc(user_poolId) {
+    //console.log(x)
+    Alert.alert(
+      "Are you sure you want to remove this user from the group?",
+      "",
+      [
+        {
+          text: "OK",
+          onPress: async () => {
+            const response = await removeGroupMember(
+              currentSession(),
+              route.params.id,
+              user_poolId
+            );
+            if (response.status == 200) {
+              alert("User removed successfully!");
+              navigation.goBack();
+            }
+          },
+        },
+        {
+          text: "Cancel",
+        },
+      ]
+    );
+  }
 
   const accordContent = [{ title: "Group Members", content: <UserList /> }];
   const image = defaultOrImage(groupImage);
@@ -178,10 +169,7 @@ const UsersInGroup = ({ navigation, route }) => {
     <ScrollView>
       <View
         style={{
-          flex: 1,
-          flexDirection: "row",
-          flexWrap: "wrap",
-          alignItems: "flex-start",
+          ...styles.containCard
         }}
       >
         <Image style={styles.imagePicker} source={image} />
@@ -200,21 +188,24 @@ const UsersInGroup = ({ navigation, route }) => {
       <View
         style={{
           flex: 1,
-          justifyContent: "flex-start",
           marginTop: "5%",
-          marginBottom: "5%",
         }}
       >
-       { owned?null: <Button
+        {owned ? null : (
+          <Button color="black" title="Exit Group" onPress={exitGroupFunc} />
+        )}
+        {owned ? (
+          <Button
+            color="black"
+            title="Delete Group"
+            onPress={removeGroupFunc}
+          />
+        ) : null}
+        <Button
           color="black"
-          title="Exit Group"
-          onPress={exitGroupFunc}
-        />}
-{ owned?(<Button
-                  color="black"
-                  title="Delete Group"
-                  onPress={removeGroupFunc}
-                />):null}
+          title="Edit Group"
+          onPress={() => alert("Function to edit group goes here")}
+        />
       </View>
     </ScrollView>
   );
@@ -234,5 +225,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#dfdfdf",
     paddingTop: 30,
     margin: 10,
+  },
+  containCard: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    margin: 10,
+    backgroundColor: "white",
+    borderRadius: 15,
+    padding: 5,
+    borderColor: "white",
+    shadowColor: "#470000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    elevation: 1,
   },
 });
